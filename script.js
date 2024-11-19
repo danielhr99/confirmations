969df95d14cb73785bc58d2547c48a06f4e305e4
danielhr99 <danielhr.stu@gmail.com>
2024-11-19 17:18:07 +0200

    script.js

--- script.js
+++ script.js
@@ -10,31 +10,36 @@ document.getElementById('rsvp-form').addEventListener('submit', function (event)
     responseMessage.textContent = "";
 
     // בדיקת שדות
-    if (!phone) {
+    if (!phone || !guests) {
         responseMessage.style.color = 'red';
-        responseMessage.textContent = "יש להזין מספר פלאפון.";
-        responseMessage.style.display = 'block'; // הצגת ההודעה
+        responseMessage.textContent = "אנא מלא את כל השדות.";
+        responseMessage.style.display = 'block';
         return;
     }
 
-    if (!guests) {
-        responseMessage.style.color = 'red';
-        responseMessage.textContent = "יש להזין כמות מגיעים.";
-        responseMessage.style.display = 'block'; // הצגת ההודעה
-        return;
-    }
-
-    // הודעת הצלחה
-    responseMessage.style.color = 'green';
-    responseMessage.textContent = "תודה על המענה, נפגש על הרחבה!";
-    responseMessage.style.display = 'block'; // הצגת ההודעה
+    // שליחת הנתונים ל-Google Sheets דרך ה-Web App
+    fetch("https://script.google.com/macros/s/AKfycbzgDwSrNXVELnq5WpT21vJ_RHrdGBTvz8DK78-xQVmWdgFnltMu81-W670_CxqZv74Q/exec", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `phone=${phone}&guests=${guests}`,
+    })
+        .then((response) => response.text())
+        .then((data) => {
+            if (data === "Success") {
+                responseMessage.style.color = 'green';
+                responseMessage.textContent = "תודה על המענה, נפגש על הרחבה!";
+                responseMessage.style.display = 'block';
+            } else {
+                responseMessage.style.color = 'red';
+                responseMessage.textContent = "אירעה שגיאה. נסה שוב.";
+                responseMessage.style.display = 'block';
+            }
+        })
+        .catch((error) => {
+            responseMessage.style.color = 'red';
+            responseMessage.textContent = "אירעה שגיאה. נסה שוב.";
+            responseMessage.style.display = 'block';
+        });
 });
-
-
-fetch("https://script.googleusercontent.com/macros/echo?user_content_key=a4OGktoKHkryS6rEAy2MjJoYEFosxRL9stgzASpOuFtM50DZTBisIqjTiX4toPCLdBFqOtFdfU_TKs-qp3_tsrybcpBOa0p1m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDbQcdmA4g6lcSQ44Z80x5n_r87IxuJePa6ng9h8BZU_MGBC1tL2veoWQN4vw1bDnoV1DK5yFsqbwm28H6SIs1N7qroWavlf0g&lib=MeEesZBY6tb6j1JOvRFREC9oOYNoxojT-", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: `phone=${phone}&guests=${guests}`,
-})
