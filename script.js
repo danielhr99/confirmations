266aaf8bb51bb4f98d5b7a7fef5fc9727a362371
danielhr99 <danielhr.stu@gmail.com>
2024-11-19 17:15:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,12 @@ document.getElementById('rsvp-form').addEventListener('submit', function (event)
     responseMessage.textContent = "תודה על המענה, נפגש על הרחבה!";
     responseMessage.style.display = 'block'; // הצגת ההודעה
 });
+
+
+fetch("https://script.googleusercontent.com/macros/echo?user_content_key=a4OGktoKHkryS6rEAy2MjJoYEFosxRL9stgzASpOuFtM50DZTBisIqjTiX4toPCLdBFqOtFdfU_TKs-qp3_tsrybcpBOa0p1m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDbQcdmA4g6lcSQ44Z80x5n_r87IxuJePa6ng9h8BZU_MGBC1tL2veoWQN4vw1bDnoV1DK5yFsqbwm28H6SIs1N7qroWavlf0g&lib=MeEesZBY6tb6j1JOvRFREC9oOYNoxojT-", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `phone=${phone}&guests=${guests}`,
+})
